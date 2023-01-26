d65e4837802bbff758f51fd7ebf6e781b5747351
luiz-08 <luiz.gustavo2928@gmail.com>
2023-01-26 09:54:40 -0800

    Ajuste nas Fun de retorno das activity e limpeza das WebView, adição da logo e ajuste

--- app/src/main/java/com/example/totemdesbloqueio/screenDesbloqueio.kt
+++ app/src/main/java/com/example/totemdesbloqueio/screenDesbloqueio.kt
@@ -5,18 +5,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Button
-import com.example.totemdesbloqueio.databinding.ActivityMainBinding
 import android.widget.Toast;
 
 
 class screenDesbloqueio : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_screen_desbloqueio)
@@ -24,16 +21,18 @@ class screenDesbloqueio : AppCompatActivity() {
         val webView: WebView = findViewById(R.id.webViewDesbloqueio)
         webView.settings.javaScriptEnabled = true
         webView.settings.domStorageEnabled = true
-        webView.loadUrl("https://github.com/luiz-08") // link to your company's portal to unlock
+        webView.loadUrl("http://desbloqueio.ambev.com.br/") // link to your company's portal to unlock
         webView.webViewClient = WebViewClient()
+    }
 
-        fun returnhome (view: View){
-            val button = findViewById<Button>(R.id.ButtonFinalizarDesbloqueio)
-            webView.clearFormData();
-            webView.clearHistory();
-            webView.clearCache(true);
-            finish();
-        }
+    fun returnhomedesbloqueio (view: View, webView: WebView){
+        webView.clearHistory();
+        webView.clearCache(true);
+        webView.clearFormData()
+
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish();
     }
 }
 
--- app/src/main/java/com/example/totemdesbloqueio/screenReset.kt
+++ app/src/main/java/com/example/totemdesbloqueio/screenReset.kt
@@ -1,12 +1,20 @@
 package com.example.totemdesbloqueio
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.TextKeyListener.clear
 import android.view.View
+import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Button
-import com.example.totemdesbloqueio.databinding.ActivityMainBinding
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+
+
 
 class screenReset : AppCompatActivity() {
 
@@ -14,18 +22,20 @@ class screenReset : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_screen_reset)
 
-        val webView: WebView = findViewById(R.id.webViewReset)
+        val webView: WebView = findViewById<WebView>(R.id.webViewReset)
         webView.settings.javaScriptEnabled = true
         webView.settings.domStorageEnabled = true
-        webView.loadUrl("https://github.com/luiz-08") // link to your company's portal to reset profile
+        webView.loadUrl("http://resetdesenha.ambev.com.br/") // link to your company's portal to reset profile
         webView.webViewClient = WebViewClient()
+    }
 
-        fun returnhome2 (view: View){
-            val button = findViewById<Button>(R.id.ButtonFinalizarReset)
-            webView.clearFormData();
-            webView.clearHistory();
-            webView.clearCache(true);
-            finish();
-        }
+    fun returnhomereset (view: View, webView: WebView){
+        webView.clearHistory();
+        webView.clearCache(true);
+        webView.clearFormData()
+
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish();
     }
 }
